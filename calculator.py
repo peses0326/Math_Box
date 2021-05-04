c8171135eebcd63c55dfbb46d1bd5e6a2bea1e30
park <park@naver.com>
2021-05-04 18:19:25 +0900

    Add get_Median fuc

--- calculator.py
+++ calculator.py
@@ -1,15 +1,19 @@
 # 기본 계산기 2
 def add(a, b):
-  return a+b
+    return a+b
 
 
 def subtract(a, b):
-  return a-b
+    return a-b
 
 
 def multiply(a, b):
-  return a*b
+    return a*b
 
 
 def divide_premium(a, b):
     return a/b
+
+
+def get_Median(a, b):
+    return (a+b)/2
